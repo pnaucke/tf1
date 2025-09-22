ae65d8500befc4297d8a113c13cd899c4242de4e
Pieter Naucke <554603@student.fontys.nl>
2025-09-22 16:28:58 +0200

    v2.8

--- main.tf
+++ main.tf
@@ -23,6 +23,36 @@ data "aws_vpc" "default" {
   default = true
 }
 
+# ----------------------
+# Subnets
+# ----------------------
+resource "aws_subnet" "web1_subnet" {
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = "172.31.1.0/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
+  tags = { Name = "web1-subnet" }
+}
+
+resource "aws_subnet" "web2_subnet" {
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = "172.31.2.0/24"
+  availability_zone       = "eu-central-1b"
+  map_public_ip_on_launch = true
+  tags = { Name = "web2-subnet" }
+}
+
+resource "aws_subnet" "db_subnet" {
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = "172.31.3.0/24"
+  availability_zone       = "eu-central-1c"
+  map_public_ip_on_launch = false
+  tags = { Name = "db-subnet" }
+}
+
+# ----------------------
+# AMI en random suffix
+# ----------------------
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
@@ -37,33 +67,6 @@ resource "random_id" "suffix" {
 }
 
 # ----------------------
-# Subnets
-# ----------------------
-resource "aws_subnet" "web1_subnet" {
-  vpc_id                  = data.aws_vpc.default.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-central-1a"
-  map_public_ip_on_launch = true
-  tags = { Name = "web1-subnet" }
-}
-
-resource "aws_subnet" "web2_subnet" {
-  vpc_id                  = data.aws_vpc.default.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "eu-central-1b"
-  map_public_ip_on_launch = true
-  tags = { Name = "web2-subnet" }
-}
-
-resource "aws_subnet" "db_subnet" {
-  vpc_id                  = data.aws_vpc.default.id
-  cidr_block              = "10.0.3.0/24"
-  availability_zone       = "eu-central-1c"
-  map_public_ip_on_launch = false
-  tags = { Name = "db-subnet" }
-}
-
-# ----------------------
 # Security Groups
 # ----------------------
 resource "aws_security_group" "web_sg" {
@@ -71,15 +74,15 @@ resource "aws_security_group" "web_sg" {
   vpc_id = data.aws_vpc.default.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -103,6 +106,13 @@ resource "aws_security_group" "db_sg" {
     security_groups = [aws_security_group.web_sg.id]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -112,30 +122,60 @@ resource "aws_security_group" "db_sg" {
 }
 
 # ----------------------
-# Load Balancer
+# EC2 Instances
+# ----------------------
+resource "aws_instance" "web1" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.web1_subnet.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  key_name               = "key-0a07792759fe3dda9"
+  tags = { Name = "web1" }
+}
+
+resource "aws_instance" "web2" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.web2_subnet.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  key_name               = "key-0a07792759fe3dda9"
+  tags = { Name = "web2" }
+}
+
+resource "aws_instance" "db" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.db_subnet.id
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  key_name               = "key-0a07792759fe3dda9"
+  tags = { Name = "database" }
+}
+
+# ----------------------
+# Load Balancer voor webservers
 # ----------------------
 resource "aws_lb" "web_lb" {
   name               = "web-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.web_sg.id]
   subnets            = [aws_subnet.web1_subnet.id, aws_subnet.web2_subnet.id]
+  security_groups    = [aws_security_group.web_sg.id]
 }
 
 resource "aws_lb_target_group" "web_tg" {
   name        = "web-tg"
   port        = 80
   protocol    = "HTTP"
-  target_type = "instance"
   vpc_id      = data.aws_vpc.default.id
+  target_type = "instance"
 
   health_check {
-    enabled             = true
-    healthy_threshold   = 2
-    interval            = 30
-    matcher             = "200"
     path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
     timeout             = 5
+    healthy_threshold   = 2
     unhealthy_threshold = 2
   }
 }
@@ -152,45 +192,16 @@ resource "aws_lb_listener" "web_listener" {
 }
 
 # ----------------------
-# EC2 Instances
+# Webservers koppelen aan target group
 # ----------------------
-resource "aws_instance" "web1" {
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.web1_subnet.id
-  vpc_security_group_ids  = [aws_security_group.web_sg.id]
-  key_name               = "key-0a07792759fe3dda9"
-  user_data              = <<-EOF
-              #!/bin/bash
-              echo "Hello World" > /var/www/html/index.html
-              yum install -y httpd
-              systemctl start httpd
-              systemctl enable httpd
-              EOF
-  tags = { Name = "web1" }
+resource "aws_lb_target_group_attachment" "web1_attach" {
+  target_group_arn = aws_lb_target_group.web_tg.arn
+  target_id        = aws_instance.web1.id
+  port             = 80
 }
 
-resource "aws_instance" "web2" {
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.web2_subnet.id
-  vpc_security_group_ids  = [aws_security_group.web_sg.id]
-  key_name               = "key-0a07792759fe3dda9"
-  user_data              = <<-EOF
-              #!/bin/bash
-              echo "Hello World" > /var/www/html/index.html
-              yum install -y httpd
-              systemctl start httpd
-              systemctl enable httpd
-              EOF
-  tags = { Name = "web2" }
-}
-
-resource "aws_instance" "db" {
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.db_subnet.id
-  vpc_security_group_ids  = [aws_security_group.db_sg.id]
-  key_name               = "key-0a07792759fe3dda9"
-  tags = { Name = "database" }
+resource "aws_lb_target_group_attachment" "web2_attach" {
+  target_group_arn = aws_lb_target_group.web_tg.arn
+  target_id        = aws_instance.web2.id
+  port             = 80
 }
