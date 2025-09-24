26a504cfb28ae3fcbc67bb06f87ee98a7d5d3e26
Pieter Naucke <554603@student.fontys.nl>
2025-09-24 09:45:32 +0200

    v3.1

--- main.tf
+++ main.tf
@@ -1,6 +1,100 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
 # ----------------------
-# Security Group DB aanpassen (alleen webservers mogen inloggen)
+# Default VPC
 # ----------------------
+data "aws_vpc" "default" {
+  default = true
+}
+
+# ----------------------
+# Subnets
+# ----------------------
+resource "aws_subnet" "web1_subnet" {
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = "172.31.1.0/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
+  tags = { Name = "web1-subnet" }
+}
+
+resource "aws_subnet" "web2_subnet" {
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = "172.31.2.0/24"
+  availability_zone       = "eu-central-1b"
+  map_public_ip_on_launch = true
+  tags = { Name = "web2-subnet" }
+}
+
+resource "aws_subnet" "db_subnet" {
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = "172.31.3.0/24"
+  availability_zone       = "eu-central-1c"
+  map_public_ip_on_launch = false
+  tags = { Name = "db-subnet" }
+}
+
+# ----------------------
+# AMI en random suffix
+# ----------------------
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "random_id" "suffix" {
+  byte_length = 2
+}
+
+# ----------------------
+# Security Groups
+# ----------------------
+resource "aws_security_group" "web_sg" {
+  name   = "web-sg-${random_id.suffix.hex}"
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_security_group" "db_sg" {
   name   = "db-sg-${random_id.suffix.hex}"
   vpc_id = data.aws_vpc.default.id
@@ -21,7 +115,7 @@ resource "aws_security_group" "db_sg" {
 }
 
 # ----------------------
-# RDS Database (vervangt de EC2 database)
+# RDS Database
 # ----------------------
 resource "aws_db_subnet_group" "db_subnet_group" {
   name       = "db-subnet-group"
@@ -36,7 +130,7 @@ resource "aws_db_instance" "db" {
   instance_class          = "db.t3.micro"
   db_name                 = "myappdb"
   username                = "admin"
-  password                = "SuperSecret123!"   # (=> later vervangen met Secret Manager)
+  password                = "SuperSecret123!"
   parameter_group_name    = "default.mysql8.0"
   skip_final_snapshot     = true
   vpc_security_group_ids  = [aws_security_group.db_sg.id]
@@ -45,7 +139,7 @@ resource "aws_db_instance" "db" {
 }
 
 # ----------------------
-# User Data voor webservers (NGINX + DB env vars)
+# User Data (Nginx + DB vars)
 # ----------------------
 locals {
   user_data = <<-EOT
@@ -64,6 +158,9 @@ locals {
   EOT
 }
 
+# ----------------------
+# Webservers
+# ----------------------
 resource "aws_instance" "web1" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t2.micro"
@@ -85,7 +182,59 @@ resource "aws_instance" "web2" {
 }
 
 # ----------------------
-# Outputs (handig!)
+# Load Balancer
+# ----------------------
+resource "aws_lb" "web_lb" {
+  name               = "web-lb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.web1_subnet.id, aws_subnet.web2_subnet.id]
+  security_groups    = [aws_security_group.web_sg.id]
+}
+
+resource "aws_lb_target_group" "web_tg" {
+  name        = "web-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+  target_type = "instance"
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.web_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web1_attach" {
+  target_group_arn = aws_lb_target_group.web_tg.arn
+  target_id        = aws_instance.web1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "web2_attach" {
+  target_group_arn = aws_lb_target_group.web_tg.arn
+  target_id        = aws_instance.web2.id
+  port             = 80
+}
+
+# ----------------------
+# Outputs
 # ----------------------
 output "load_balancer_dns" {
   value = aws_lb.web_lb.dns_name
